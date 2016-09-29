78340713c385449b1d175d6aea89719c1f264083
victorsun123 <victor7sun@gmail.com>
2016-09-28 17:34:13 -0700

    basic layout

--- app/src/main/java/mdb/pokedex/MainActivity.java
+++ app/src/main/java/mdb/pokedex/MainActivity.java
@@ -2,6 +2,8 @@ package mdb.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
