892b56473249f1bd83855f2a297371ef54f9bbb8
victorsun123 <victor7sun@gmail.com>
2016-09-28 23:17:36 -0700

    Recycler view working :)

---
+++ app/src/main/java/mdb/pokedex/GridActivity.java
@@ -0,0 +1,18 @@
+package mdb.pokedex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
